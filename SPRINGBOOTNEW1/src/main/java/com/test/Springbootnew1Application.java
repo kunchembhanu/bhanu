b6c7f981d0dc645c8f16b8fb36467914c52d74f1
AM <AM@DESKTOP-UE5TKLO>
2022-09-12 18:26:07 +0530

    SpringJDBC

---
+++ SPRINGBOOTNEW1/src/main/java/com/test/Springbootnew1Application.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootnew1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootnew1Application.class, args);
+	}
+
+}
