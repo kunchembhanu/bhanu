fc7a075dadd140e64bebbfc251f556bea293d390
AM <AM@DESKTOP-UE5TKLO>
2022-11-01 12:04:40 +0530

    Hibernate Example without annotations

---
+++ HybernateExp1/src/main/java/bhanu/com/Test.java
@@ -0,0 +1,37 @@
+package bhanu.com;
+
+import org.hibernate.Session;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+
+	public static void main(String[] args) {
+Configuration cfg = new Configuration();
+		
+		cfg.configure("hibernate.cfg.xml");
+		
+		SessionFactory sf = cfg.buildSessionFactory();
+		
+		Session session = sf.openSession();
+		
+		Transaction ts = session.beginTransaction();
+		
+		Customer cobj = new Customer();
+		
+		cobj.setOrdername("bhanu");
+		cobj.setLocation("PNG");
+		
+		session.persist(cobj);
+		
+		ts.commit();
+		
+		session.close();
+		
+		System.out.println("Done.");
+	}
+	}
+
+
