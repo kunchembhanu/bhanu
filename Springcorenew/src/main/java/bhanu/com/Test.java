cf36145b26aef4e8c21bed6ceabb2283758b8ed3
AM <AM@DESKTOP-UE5TKLO>
2022-11-07 13:32:39 +0530

    SpringCore

---
+++ Springcorenew/src/main/java/bhanu/com/Test.java
@@ -0,0 +1,23 @@
+package bhanu.com;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		Resource rs = new ClassPathResource("applicationContext.xml");
+		
+		BeanFactory bn  = new XmlBeanFactory(rs);
+		
+		Employee emp = (Employee)bn.getBean("emp");
+		
+		emp.disp();
+	}
+
+}
+
+
