18da6354070f910afc923033f3c658ec733fd880
AM <AM@DESKTOP-UE5TKLO>
2022-12-02 17:17:59 +0530

    SpringBOOTSimple

---
+++ SpringbootSimple/src/main/java/com/bhanu/spring/SpringbootBhanuApplication.java
@@ -0,0 +1,14 @@
+package com.bhanu.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBhanuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBhanuApplication.class, args);
+		System.out.println("Fine.");
+	}
+
+}
---
+++ SpringbootSimple/src/test/java/com/bhanu/spring/SpringbootBhanuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhanu.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBhanuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
